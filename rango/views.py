451b8c14542bd8e5ac3249a21aa12c2e46f889ce
Jerhan Cheng <2399630c@student.gla.ac.uk>
2019-02-08 22:40:22 +0000

    first test

---
+++ rango/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+# Import the Category model
+from rango.models import Category
+
+def index(request):
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
